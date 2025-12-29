7ad4287f88f179746782d0f4f41c4748e74b48ab
Kolchi <kolchiibrahim@icloud.com>
2025-12-29 18:22:18 +0400

    Add files via upload

---
+++ CarRentalApp/CarRentalApp/Document.swift
@@ -0,0 +1,21 @@
+//
+//  Document.swift
+//  CarRentalApp
+//
+//  Created by Kolchı Ibrahım on 25.12.25.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
---
+++ CarRentalApp/CarRentalApp/DocumentViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DocumentViewController.swift
+//  CarRentalApp
+//
+//  Created by Kolchı Ibrahım on 25.12.25.
+//
+
+import UIKit
+
+class DocumentViewController: UIDocumentViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
